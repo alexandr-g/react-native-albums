e271c2e36a3c53a5f1c106da56933e0026972ca9
Alexandr <alexgrischuk@gmail.com>
2017-01-08 18:29:25 +0100

    Fix jest tests for ios

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import AlbumList from '../src/components/AlbumList';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+describe('basic components render', function() {
+  it('should render AlbumList correctly', () => {
+    const tree = renderer.create(
+      <AlbumList />
+    );
+  });
 });
