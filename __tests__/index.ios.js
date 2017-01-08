f2e25f2c541517dd560b24181c6b617c0c521d42
Alexandr <alexgrischuk@gmail.com>
2017-01-08 18:34:14 +0100

    Add test for Card component

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -3,11 +3,18 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import AlbumList from '../src/components/AlbumList';
+import Card from '../src/components/Card';
 
 describe('basic components render', function () {
   it('should render AlbumList correctly', () => {
     const tree = renderer.create(
       <AlbumList />
+    );
+  });
+
+  it('should render Card correctly', () => {
+    const tree = renderer.create(
+      <Card />
     );
   });
 });
